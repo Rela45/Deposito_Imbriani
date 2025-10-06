e1b2a218791825f1a5aebcec2ecacc77018b57e9
Alessandro Imbriani <alex.imbriani00@gmail.com>
2025-10-06 11:18:27 +0200

    Add files via upload

---
+++ AgeHeight/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine($"how old are u? ");
+string? input1 = Console.ReadLine();
+if (string.IsNullOrEmpty(input1))
+{
+    Console.WriteLine("You did not enter a number!");
+    return;
+}
+int age = int.Parse(input1);
+Console.WriteLine($"how tall are u? (in meters) ");
+string? input2 = Console.ReadLine();
+if (string.IsNullOrEmpty(input2))
+{
+    Console.WriteLine("You did not enter a number!");
+    return;
+}
+float height = float.Parse(input2);
+
+int sum = age + (int)height;
+Console.WriteLine($"sum of your age and height is: {sum}  ");
+
+
