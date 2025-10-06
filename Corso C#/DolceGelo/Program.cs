f1da4dc56631c49d26fce3d05fb84ebc149553b8
Rela45 <alex.imbriani00@gmail.com>
2025-10-06 14:10:30 +0200

    Update
    ho cancellato tutto il progetto e l'ho rifatto da capo

---
+++ Corso C#/DolceGelo/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Runtime.CompilerServices;
+
+
+namespace DolceGelo
+{
+    class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            string[] gusti = ["Cioccolato", "Vaniglia", "Fragola", "Pistacchio", "Limone"];
+            double[] prezzi = [1.50, 1.20, 1.30, 1.80, 1.10];
+
+            int scelta = 0;
+            int quantita = 0;
+            bool continua = true;
+
+            List<int> indiceGusti = new List<int>();
+
+            while (continua == true)
+            {
+                StampaMenu(gusti, prezzi);
+                Console.WriteLine("Scegli un'opzione: ");
+                scelta = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Vuoi Aggiungere un altro gusto? Y per si N per No");
+                var keyinfo = Console.ReadKey();
+                indiceGusti.Add(scelta);
+                if (keyinfo.Key == ConsoleKey.Y)
+                {
+                    quantita++;
+                    continue;
+                }
+                else
+                {
+                    continua = false;
+                }
+
+            }
+
+            
+
+        }
+
+        public static void StampaMenu(string[] gusti, double[] prezzi)
+        {
+            Console.WriteLine("Gusti disponibili:");
+            for (int i = 0; i < gusti.Length; i++)
+            {
+                Console.WriteLine($"{i} - {gusti[i]} ({prezzi[i].ToString("C2")})");
+            }
+            Console.WriteLine();
+        }
+
+        public static double CalcolaTotale(int indiceGusti, int quantitaGusti)
+        {
+            return 0;
+        } 
+    }
+}
