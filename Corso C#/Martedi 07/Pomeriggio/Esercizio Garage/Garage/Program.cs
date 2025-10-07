dd2b658df75f329128676bc36fcbf5307c327e4b
Rela45 <alex.imbriani00@gmail.com>
2025-10-07 16:22:37 +0200

    Esercizio Garage Completato

---
+++ Corso C#/Martedi 07/Pomeriggio/Esercizio Garage/Garage/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+
+public class Veicolo
+{
+    protected string? Marca;
+    protected string? Modello;
+
+    public Veicolo(string? marca, string? modello)
+    {
+        Marca = marca;
+        Modello = modello;
+    }
+
+    public override string? ToString()
+    {
+        return $"Marca: {Marca}, Modello: {Modello}";
+
+    }
+}
+
+
+public class Auto : Veicolo
+{
+    public Auto(string? marca, string? modello) : base(marca, modello) //sovrascrivo il costruttore
+    {
+        this.Marca = marca;
+        this.Modello = modello;
+    }
+    public override string? ToString()
+    {
+        return $"Auto - {base.ToString()}";
+    }
+    
+}   
+
+public class Moto : Veicolo
+{
+    public Moto(string? marca, string? modello) : base(marca, modello)
+    {
+        this.Marca = marca;
+        this.Modello = modello;
+    }
+    public override string ToString()
+    {
+        return $"Moto - {base.ToString()}";
+    }
+    
+}
+class Program
+{
+    public static void Main()
+    {
+        // Veicolo veicolo1 = new Auto("Fiat", "Punto");
+        // Veicolo veicolo2 = new Moto("Yamaha", "R1");
+
+        // Console.WriteLine(veicolo1.ToString());
+        // Console.WriteLine(veicolo2.ToString());
+
+        List<Veicolo> garage = new List<Veicolo>();
+        bool continua = true;
+        while(continua){
+            Console.WriteLine($"Inserisci 1 per aggiungere un'auto, 2 per aggiungere una moto, 3 per visualizzare il garage, 4 per uscire");
+            
+            int scelta = Convert.ToInt32(Console.ReadLine());
+
+            switch (scelta)
+            {
+                case 1:
+                    Console.WriteLine("Inserisci marca auto");
+                    string? marcaAuto = Console.ReadLine();
+                    Console.WriteLine("Inserisci modello auto");
+                    string? modelloAuto = Console.ReadLine();
+                    garage.Add(new Auto(marcaAuto, modelloAuto));
+                    break;
+                case 2:
+                    Console.WriteLine("Inserisci marca moto");
+                    string? marcaMoto = Console.ReadLine();
+                    Console.WriteLine("Inserisci modello moto");
+                    string? modelloMoto = Console.ReadLine();
+                    garage.Add(new Moto(marcaMoto, modelloMoto));
+                    break;
+
+
+                case 3:
+                    foreach (Veicolo veicolo in garage)
+                    {
+                        Console.WriteLine(veicolo.ToString());
+                    }
+                    break;
+
+                case 4:
+                    Console.WriteLine("Programma terminato");
+                    continua = false;
+                    break;
+            }   
+        }
+    }
+}
