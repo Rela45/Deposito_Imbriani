935d5147f0a0da9e539dcfa2e93963112fe0da78
Rela45 <alex.imbriani00@gmail.com>
2025-10-10 12:14:07 +0200

    Esercizio dell'esame
    Ho fatto l'esercizio dell'esame ma non sono riuscito a completarlo nel main per mancanza di tempo

---
+++ Corso C#/Venerdi 10/Mattina/EsercizioEsame/Program.cs
@@ -0,0 +1,57 @@
+﻿
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+    }
+}
+
+class Universita
+{
+    public string Nome;
+    public List<Studente> Studenti { get; set; }
+
+    public Universita(string nome)
+    {
+        Nome = nome;
+        Studenti = new List<Studente>();
+    }
+
+    public void AggiungiStudente(Studente studente)
+    {
+        Studenti.Add(studente);
+    }
+
+    public void StampaStudenti()
+    {
+        Console.WriteLine($"Università: {Nome}");
+        foreach (var studente in Studenti)
+        {
+            Console.WriteLine(studente);
+        }
+    }
+}
+
+// Classe Studente
+class Studente : Universita
+{
+    public string NomeStudente { get; set; }
+    public string Cognome { get; set; }
+    public string Matricola { get; set; }
+
+    public Studente(string nomeStudente, string cognome, string matricola, string nome) : base(nome)
+    {
+        NomeStudente = nomeStudente;
+        Cognome = cognome;
+        Matricola = matricola;
+    }
+
+    public override string ToString()
+    {
+        return $"Studente: {Nome} {Cognome}, Matricola: {Matricola}";
+    }
+}
+
