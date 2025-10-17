a69941365e1b2489c65fec5626f1383dade2062f
Rela45 <alex.imbriani00@gmail.com>
2025-10-17 17:06:59 +0200

    Commit per esercizio finale con Dictionary

---
+++ Corso C#/Esempi/EsDictionary/Program.cs
@@ -0,0 +1,19 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        Dictionary<int, string> studenti = new Dictionary<int, string>();
+
+        Dictionary<int, List<string>> students = new Dictionary<int, List<string>>();
+        //possiamo usare anche un dictionary di dictionary potenzialmente, accetta quindi anche altre liste
+        //per la lista possiamo crearla prima e poi la passiamo nel dictionary tramite l'add.
+        var capitali = new Dictionary<string, string>
+        {
+            { "Italia", "Roma" },
+            {"Francia", "Parigi"}
+        };
+
+        
+
+    }
+}
