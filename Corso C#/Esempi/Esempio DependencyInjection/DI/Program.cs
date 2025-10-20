809b88476d645085b2dd75c6278f61a9623460e6
Rela45 <alex.imbriani00@gmail.com>
2025-10-20 16:47:53 +0200

    commit dei due esercizi sulle dipendency injection

---
+++ Corso C#/Esempi/Esempio DependencyInjection/DI/Program.cs
@@ -0,0 +1,36 @@
+﻿public interface ILogger
+{
+    void Log(string message);
+}
+
+public class ConsoleLogger : ILogger
+{
+    public void Log(string message)
+    {
+        Console.WriteLine($"LOG: {message}");
+    }
+}
+
+public class UserService
+{
+    private readonly ILogger _logger;
+
+    public UserService(ILogger logger)  //constructor injection
+    {
+        _logger = logger;
+    }
+
+    public void CreateUser(string name)
+    {
+        _logger.Log($"User '{name}' created.");
+    }
+}
+class Program
+{
+    static void Main()
+    {
+        ILogger logger = new ConsoleLogger();
+        UserService userService = new UserService(logger);
+        userService.CreateUser("Alice");
+    }
+}
