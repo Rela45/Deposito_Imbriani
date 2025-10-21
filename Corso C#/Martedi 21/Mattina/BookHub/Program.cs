38d61bf81e10d9742592c4af40603bf39bdd3285
Rela45 <alex.imbriani00@gmail.com>
2025-10-21 11:32:54 +0200

    Commit Primo Check esercizio MEDIO
    ho inizializzato le interfacce e implementato le classi per i payment process

---
+++ Corso C#/Martedi 21/Mattina/BookHub/Program.cs
@@ -0,0 +1,55 @@
+﻿#region Interfaces
+public interface IInventoryService
+{
+    bool CheckStock(string productId, int quantity);
+    void ReserveStock(string productId, int quantity);
+}
+
+public interface IPaymentGateway
+{
+    void PaymentProcess();
+}
+
+public interface INotificationService
+{
+    void Send(string message);
+}
+
+public interface IPricingStrategy
+{
+    decimal ApplyPricing(decimal basePrice);
+}
+#endregion
+
+#region Implementation
+
+public class PaypalGateway : IPaymentGateway
+{
+    public void PaymentProcess()
+    {
+        Console.WriteLine($"Payment with Paypal");
+    }
+}
+
+public class StripeGateway : IPaymentGateway
+{
+    public void PaymentProcess()
+    {
+        Console.WriteLine($"Payment with Stripe");
+    }
+}
+
+
+#endregion
+
+
+
+#region MAIN
+class Program
+{
+    static void Main(string[] args)
+    {
+
+    }
+}
+#endregion
