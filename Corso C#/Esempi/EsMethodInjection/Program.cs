c51df86227a6e8e6182ae09a3872b596743bb625
Rela45 <alex.imbriani00@gmail.com>
2025-10-21 15:34:26 +0200

    Commit 90% esercizio 1 method injection
    devo implementare anche il setter injection oltre ai  2 gia' implementati

---
+++ Corso C#/Esempi/EsMethodInjection/Program.cs
@@ -0,0 +1,30 @@
+﻿public interface INotifier
+{
+    void Notify(string message);
+}
+
+public class EmailNotifier : INotifier
+{
+    public void Notify(string message)
+    {
+        Console.WriteLine($"Invio email: {message}");
+    }
+}
+
+public class NotificationService
+{
+    public void SendNotification(string user, INotifier notifier)
+    {
+        notifier.Notify($"Ciao {user}, hai una nuova notifica!");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        INotifier notifier = new EmailNotifier();
+        var service = new NotificationService();
+        service.SendNotification("Luca", notifier);
+    }
+}
---
+++ Corso C#/Martedi 21/Pomeriggio/AlertService/Program.cs
@@ -0,0 +1,67 @@
+﻿#region INTERFACE
+public interface INotifier
+{
+    void Notify(string message);
+}
+
+public interface ILogger
+{
+    void Log(string nome);
+}
+
+#endregion
+
+//implementazioni delle classi concrete
+#region Implementation
+public class SmsNotifier : INotifier
+{
+    public void Notify(string message)
+    {
+        Console.WriteLine($"Invio SMS : {message}");
+    }
+}
+
+public class SmsLogger : ILogger
+{
+    public void Log(string nome)
+    {
+        Console.WriteLine($"LOG {nome} :[{DateTime.Now}]");
+    }
+}
+
+#endregion
+
+
+#region Service
+
+public class AlertService
+{
+    private readonly ILogger _logger;  //variabile per constructor injection
+    public AlertService(ILogger logger)  
+    {
+        _logger = logger;
+    }
+    public void SendAlert(string message, INotifier notifier) //method injection
+    {
+        _logger.Log("Alessandro45");    //costructor injection
+        notifier.Notify(message);  
+    }
+}
+#endregion
+
+#region MAIN
+class Program
+{
+    static void Main(string[] args)
+    {
+        INotifier notifier = new SmsNotifier();
+        ILogger logger = new SmsLogger();
+        var service = new AlertService(logger);
+        service.SendAlert("Wanna duoQ?", notifier);
+
+    }
+}
+
+
+
+#endregion
