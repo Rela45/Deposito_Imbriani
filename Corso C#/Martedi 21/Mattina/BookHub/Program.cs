4b693f917a1e16690c411c96dfa170e4ca1eb7f9
Rela45 <alex.imbriani00@gmail.com>
2025-10-21 12:57:16 +0200

    Commit finale dell'esercizio
    non ho completato l'esercizio, mi sono bloccato nel main sull'aggiunta del prodotto nella mia lista. devo ragionarla diversamente

--- Corso C#/Martedi 21/Mattina/BookHub/Program.cs
+++ Corso C#/Martedi 21/Mattina/BookHub/Program.cs
@@ -1,5 +1,6 @@
 ﻿#region Interfaces
 using System.ComponentModel;
+using System.Data.Common;
 public interface IProduct
 {
     string ProductCode();
@@ -46,10 +47,15 @@ public class StripeGateway : IPaymentGateway
 
 public class Inventory : IInventoryService
 {
+    private readonly ProductFactory _product;
+    public Inventory(ProductFactory product)
+    {
+        _product = product;
+    }
     public bool CheckStock(string productId, int quantity)
     {
         Console.WriteLine($"Verifico la disponibilita del {productId} per la quantita' {quantity}");
-        return true;
+        return true; //funzione sempre true, non riesco a dare ancora una logica funzionale
     }
 
     public void ReserveStock(string productId, int quantity)
@@ -85,31 +91,48 @@ public class PushNotification : INotificationService
 
 
 #endregion
+#region Order
+public class Order
+{
+    public string Id { get; set; }
+    public decimal Prezzo { get; set; }
+    public int qty{ get; set; }
+}
 
+#endregion
 
 #region ORDERSERVICE
 public class Orderservice
 {
     private readonly IPaymentGateway _payment;
     private readonly IInventoryService _inventoryService;
+    private readonly List<Order> _ordini = new();
 
     public INotificationService notificationService { get; set; }
-    public IPricingStrategy pricingStrategy{ get; set; }
+    public IPricingStrategy pricingStrategy { get; set; }
+
+    public Email email = new Email();
 
     public Orderservice(IPaymentGateway payment, IInventoryService inventoryService)
     {
         _payment = payment;
         _inventoryService = inventoryService;
     }
+    public void AddOrder(Order ordine)
+    {
+        if (_inventoryService.CheckStock(ordine.Id, ordine.qty))
+        {
+            _ordini.Add(ordine);
+            email.Send("Ordine Aggiunto correttamente");
+        }
+    }
 
     public void CreatePayment()
     {
         _payment.PaymentProcess();
+        email.Send("Pagamento riuscito");
     }
-    public void BookOrder()
-    {
-        // if(_inventoryService.CheckStock(string productId, int quantity))
-    }
+
 }
 
 #endregion
@@ -137,10 +160,26 @@ public abstract class ProductFactory
 {
     public abstract IProduct FactoryMethod();
 
-    public void CreateProduct()
+    public string CreateProduct()
     {
         IProduct product = FactoryMethod();
-        product.ProductCode();
+        return product.ProductCode();
+    }
+}
+
+public class RealDigitalBook : ProductFactory
+{
+    public override IProduct FactoryMethod()
+    {
+        return new DigitalBook();
+    }
+}
+
+public class RealPrintBook : ProductFactory
+{
+    public override IProduct FactoryMethod()
+    {
+        return new PrintedBook();
     }
 }
 
@@ -151,6 +190,14 @@ class Program
 {
     static void Main(string[] args)
     {
+        ProductFactory book = new RealDigitalBook();
+        book.CreateProduct();
+        IInventoryService inventoryService = new Inventory(book);
+        IPaymentGateway paymentGateway = new PaypalGateway();
+
+        var orderService = new Orderservice(paymentGateway, inventoryService);
+
+        // orderService.AddOrder(new Order {Id= book})  non riesco a prendere il product code e inserirlo come id dell'ordine, forse devo ragionarla diversamente
 
     }
 }
