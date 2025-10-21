3a85fbbe670c2d863359c778e667ea52aa47d61a
Rela45 <alex.imbriani00@gmail.com>
2025-10-21 12:04:23 +0200

    Update secondo Check
    sono in fase di creazione del factory method, senza quello non posso completare l'order service

--- Corso C#/Martedi 21/Mattina/BookHub/Program.cs
+++ Corso C#/Martedi 21/Mattina/BookHub/Program.cs
@@ -1,4 +1,9 @@
 ﻿#region Interfaces
+using System.ComponentModel;
+public interface IProduct
+{
+    string ProductCode();
+}
 public interface IInventoryService
 {
     bool CheckStock(string productId, int quantity);
@@ -39,10 +44,107 @@ public class StripeGateway : IPaymentGateway
     }
 }
 
+public class Inventory : IInventoryService
+{
+    public bool CheckStock(string productId, int quantity)
+    {
+        Console.WriteLine($"Verifico la disponibilita del {productId} per la quantita' {quantity}");
+        return true;
+    }
+
+    public void ReserveStock(string productId, int quantity)
+    {
+        Console.WriteLine($"Riservo il {productId} in stock di {quantity}");
+    }
+}
+
+public class Email : INotificationService
+{
+    public void Send(string message)
+    {
+        Console.WriteLine($"[{DateTime.Now:HH:MM:ss}]: {message} inviato tramite Mail");
+    }
+}
+
+public class SMS : INotificationService
+{
+    public void Send(string message)
+    {
+        Console.WriteLine($"[{DateTime.Now:HH:MM:ss}]: {message} inviato tramite SMS");
+    }
+}
+
+public class PushNotification : INotificationService
+{
+    public void Send(string message)
+    {
+        Console.WriteLine($"[{DateTime.Now:HH:MM:ss}]: {message} :Notifica inviata");
+    }
+}
+
+
 
 #endregion
 
 
+#region ORDERSERVICE
+public class Orderservice
+{
+    private readonly IPaymentGateway _payment;
+    private readonly IInventoryService _inventoryService;
+
+    public INotificationService notificationService { get; set; }
+    public IPricingStrategy pricingStrategy{ get; set; }
+
+    public Orderservice(IPaymentGateway payment, IInventoryService inventoryService)
+    {
+        _payment = payment;
+        _inventoryService = inventoryService;
+    }
+
+    public void CreatePayment()
+    {
+        _payment.PaymentProcess();
+    }
+    public void BookOrder()
+    {
+        // if(_inventoryService.CheckStock(string productId, int quantity))
+    }
+}
+
+#endregion
+#region ConcreteClasses
+public class DigitalBook : IProduct
+{
+    public string ProductCode()
+    {
+        return "BOOK_DIGITAL";
+    }
+}
+
+public class PrintedBook : IProduct
+{
+    public string ProductCode()
+    {
+        return "BOOK_PRINT";
+    }
+}
+
+#endregion
+
+#region Factory
+public abstract class ProductFactory
+{
+    public abstract IProduct FactoryMethod();
+
+    public void CreateProduct()
+    {
+        IProduct product = FactoryMethod();
+        product.ProductCode();
+    }
+}
+
+#endregion
 
 #region MAIN
 class Program
